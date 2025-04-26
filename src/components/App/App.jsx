ad1075ae6eda3c89de56e33d59c24f6dd112b84b
OksanaNova <o.nova0804@gmail.com>
2025-04-25 23:08:29 -0600

    structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,21 @@
+import AppRoutes from "../Routes/AppRoutes";
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Sidebar from '../Sidebar/Sidebar';
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div className="app">
+            <Header />
+
+            <div className="container">
+                <Sidebar />
+                <AppRoutes />
+            </div>
+            
+            <Footer />
+        </div>
+    )
 };
 
 export default App;
