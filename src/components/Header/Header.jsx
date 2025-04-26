16e83b03a4e2a85a1b420d9cbc5e2cb309cae049
OksanaNova <o.nova0804@gmail.com>
2025-04-25 23:56:46 -0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,40 @@
+import { Link } from "react-router-dom";
+
+import styles from '../../styles/Header.module.css';
+
+import { ROUTES } from '../../utils/routes';
+
+import LOGO from '../../images/logo.svg';
+import AVATAR from 
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className={styles.header}>
+
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="Stuff" />
+                </Link>
+            </div>
+
+            <div className={styles.info}>
+                <div className={styles.user}>
+                    <div 
+                        className={styles.avatar} 
+                        style={{ backgroundImage: `url(${AVATAR})` }} 
+                    />
+                    <div className={styles.username}>
+                        
+                    </div>
+                </div>
+
+                <form className={styles.form}>
+                    <div className={styles.icon}></div>
+                    
+                </form>
+            </div>
+
+        </div>
     )
 }
 
