bb6fa63db8a37cf61dfc0bed64fa4282417b08d2
OksanaNova <o.nova0804@gmail.com>
2025-04-29 21:16:13 -0600

    categoriesSlice

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import categoriesSlise from './categories/categoriesSlice';
+
 export const store = configureStore({
     reducer: {
-        categories: 'cat',
+        categories: categoriesSlise,
     },
     devTools: true,
 });
