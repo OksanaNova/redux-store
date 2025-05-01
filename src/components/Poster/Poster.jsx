ba8be5acfdffba81a3a4b5f81b943f6581aaa54b
OksanaNova <o.nova0804@gmail.com>
2025-05-01 09:30:50 -0600

    poster added

---
+++ src/components/Poster/Poster.jsx
@@ -0,0 +1,23 @@
+import styles from '../../styles/Home.module.css';
+
+import BG from '../../images/computer.png';
+
+const Poster = () => {
+    return (
+        <section className={styles.home}>
+            <div className={styles.title}>BIG SALE UP TO 50%</div>
+            <div className={styles.product}>
+                <div className={styles.text}>
+                    <div className={styles.subtitle}>THE BESTSELLER OF 2025</div>
+                    <h1 className={styles.head}>LENNON r2d2 <br/> with NVIDIA 5090 TI</h1>
+                    <button className={styles.button}>Shop Now</button>
+                </div>
+                <div className={styles.image}>
+                    <img src={BG} alt='computer' />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Poster
