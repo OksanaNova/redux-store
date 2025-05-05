118cdc19538cea31b89c8cd3159df64960d5aa15
OksanaNova <o.nova0804@gmail.com>
2025-05-05 11:26:24 -0600

    styles added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,23 @@
+import { useSelector } from "react-redux";
+
+import styles from '../../styles/Home.module.css';
+
 import Poster from "../Poster/Poster";
+import Products from "../Products/Products";
 
 const Home = () => {
+
+    // const { list } = useSelector(({ products }) => products)
+    const products = useSelector((state) => state.products);
+    const list = products.list;
+
+
+
     return (
-        <>
+        <div className={styles.home}>
             <Poster />
-        </>
+            <Products products={list} amount={5} title="Trending"/>
+        </div>
     )
 }
 
